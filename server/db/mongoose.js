079e2636a1cafb8920a511559c78fb4bbac49cba
parth kakadiya <parth.kakadiya1208@gmail.com>
2018-05-08 18:18:05 +0200

    Structural Change

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,6 +5,6 @@ let db = {
     localhost: 'mongodb://localhost:27017/TodoApp',
     mlab: 'mongodb://parth:123@ds217560.mlab.com:17560/todoapp'
 };
-mongoose.connect( db.localhost || db.mlab);
+mongoose.connect(db.localhost || db.mlab);
 
 module.exports  = {mongoose};
