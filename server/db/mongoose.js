cbbebc0d004b1678c7b552fcd742fa1a6a926307
parth kakadiya <parth.kakadiya1208@gmail.com>
2018-05-09 11:42:50 +0200

    Database User Changed

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 let db = {
     localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: 'mongodb://parth:123@ds217560.mlab.com:17560/todoapp'
+    mlab: 'mongodb://pmk:111@ds217560.mlab.com:17560/todoapp'
 };
 mongoose.connect(db.localhost || db.mlab);
 
