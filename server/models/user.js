d5b5bd096986f825128f110b67982a2734483ca0
parth kakadiya <parth.kakadiya1208@gmail.com>
2018-05-11 17:54:44 +0200

    ADD GET users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -45,11 +45,27 @@ UserSchema.methods.generateAuthToken = function () {
     const user = this;
     const access = 'auth';
     const token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
-
+    console.log(user);
     user.tokens = user.tokens.concat({access, token});
 
     return user.save().then(() => {
         return token;
+    });
+};
+UserSchema.statics.findByToken = function (token) {
+    const User = this;
+    let decoded;
+
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch (e) {
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
     });
 };
 const User = mongoose.model('User', UserSchema);
