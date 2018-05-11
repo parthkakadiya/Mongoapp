a6b69305b9466dfd0af46cba411955594905dfdb
parth kakadiya <parth.kakadiya1208@gmail.com>
2018-05-11 18:13:34 +0200

    Hash User Password Befor Save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 const data = {
     id: 10
 };
+// const password = '123abc!';
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-const token = jwt.sign(data, '123abc');
-console.log(token);
+const hashedPassword = '$2a$10$huAU4qTnQuGPifHEXfV9cOmPJ7p61oKaoXrY1WviiDAznE/rW8oLK';
 
-const decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+bcrypt.compare('123ab!', hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+
+
+// const token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// const decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 // const msg = 'I am Parth';
 //
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 //Schema for Database
 
@@ -68,6 +69,20 @@ UserSchema.statics.findByToken = function (token) {
         'tokens.access': 'auth'
     });
 };
+UserSchema.pre('save', function (next) {
+    const user = this;
+
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
 const User = mongoose.model('User', UserSchema);
 
 module.exports  = {User};
