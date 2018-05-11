f3c9fbbf080ab8338ab87f20d7e0ad691b639623
parth kakadiya <parth.kakadiya1208@gmail.com>
2018-05-11 16:57:32 +0200

    Add POST /users and token method

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-let db = {
-    localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: 'mongodb://pmk:111@ds217560.mlab.com:17560/todoapp'
-};
-mongoose.connect(db.mlab || db.localhost);
+
+mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports  = {mongoose};
